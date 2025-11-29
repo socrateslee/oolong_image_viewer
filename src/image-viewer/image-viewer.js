7762966d325e17724cdfd6a3c730562a9acb44a2
socrateslee <ubuntu@DESKTOP-HN4Q9NH.>
2025-11-29 15:43:13 +0800

    support SVG

--- src/image-viewer/image-viewer.js
+++ src/image-viewer/image-viewer.js
@@ -1,23 +1,38 @@
 function ImageViewer(img){
+    var isSVG = img.tagName === 'SVG' || img.tagName === 'svg';
+
     this.startDrag = function(e){
         if (!e) {
             var e = window.event;
         }
-		if (e.target.tagName != 'IMG') return;
+		// For SVG, the target might be a child element, so check if it's within the img element
+		var targetElement = e.target;
+		var isValidTarget = false;
+
+		if (isSVG) {
+			// For SVG, check if target is the SVG itself or a child of it
+			isValidTarget = (targetElement === img || img.contains(targetElement));
+		} else {
+			// For IMG, check if target is the image
+			isValidTarget = (targetElement.tagName === 'IMG');
+		}
+
+		if (!isValidTarget) return;
+
         offsetX = e.clientX;
         offsetY = e.clientY;
 
-        if(!e.target.style.left) e.target.style.left='0px';
-        if(!e.target.style.top) e.target.style.top='0px';
+        if(!img.style.left) img.style.left='0px';
+        if(!img.style.top) img.style.top='0px';
 
-        var coordX = parseInt(e.target.style.left);
-        var coordY = parseInt(e.target.style.top);
+        var coordX = parseInt(img.style.left);
+        var coordY = parseInt(img.style.top);
 
 		img.style.cursor = "-webkit-grabbing";
         document.onmousemove = function(e){
             if (!e) {var e= window.event};
-            e.target.style.left = coordX + e.clientX - offsetX + 'px';
-            e.target.style.top = coordY + e.clientY - offsetY + 'px';
+            img.style.left = coordX + e.clientX - offsetX + 'px';
+            img.style.top = coordY + e.clientY - offsetY + 'px';
             return false;
         };
         return false;
@@ -39,35 +54,72 @@ function ImageViewer(img){
     }
 
 	this.zoomin = function(){
-		img.width = parseInt(img.width * 1.25);
-		img.height = parseInt(img.height * 1.25);
+		if (isSVG) {
+			var currentWidth = img.clientWidth || parseInt(img.getAttribute('width')) || 300;
+			var currentHeight = img.clientHeight || parseInt(img.getAttribute('height')) || 300;
+			img.setAttribute('width', parseInt(currentWidth * 1.25));
+			img.setAttribute('height', parseInt(currentHeight * 1.25));
+		} else {
+			img.width = parseInt(img.width * 1.25);
+			img.height = parseInt(img.height * 1.25);
+		}
 	}
 
 	this.zoomout = function(){
-		img.width = parseInt(img.width * 0.8);
-		img.height = parseInt(img.height * 0.8);
+		if (isSVG) {
+			var currentWidth = img.clientWidth || parseInt(img.getAttribute('width')) || 300;
+			var currentHeight = img.clientHeight || parseInt(img.getAttribute('height')) || 300;
+			img.setAttribute('width', parseInt(currentWidth * 0.8));
+			img.setAttribute('height', parseInt(currentHeight * 0.8));
+		} else {
+			img.width = parseInt(img.width * 0.8);
+			img.height = parseInt(img.height * 0.8);
+		}
 	}
 
 	this.original = function(fitWindow){
-    if(img.naturalWidth < window.innerWidth || fitWindow != true){
-		    img.width = img.naturalWidth;
-		    img.height = img.naturalHeight;
-    }
-    else{
-        img.width = window.innerWidth * 0.8;
-        img.height = img.naturalHeight * window.innerWidth * 0.8 / img.naturalWidth
-    }
+		if (isSVG) {
+			var originalWidth, originalHeight;
+
+			// Try to get dimensions from viewBox first
+			if (img.viewBox && img.viewBox.baseVal && img.viewBox.baseVal.width > 0) {
+				originalWidth = img.viewBox.baseVal.width;
+				originalHeight = img.viewBox.baseVal.height;
+			} else {
+				// Fallback to width/height attributes or computed size
+				originalWidth = parseInt(img.getAttribute('width')) || img.clientWidth || 300;
+				originalHeight = parseInt(img.getAttribute('height')) || img.clientHeight || 300;
+			}
+
+			if(originalWidth < window.innerWidth || fitWindow != true){
+				img.setAttribute('width', originalWidth);
+				img.setAttribute('height', originalHeight);
+			}
+			else{
+				img.setAttribute('width', window.innerWidth * 0.8);
+				img.setAttribute('height', originalHeight * window.innerWidth * 0.8 / originalWidth);
+			}
+		} else {
+			if(img.naturalWidth < window.innerWidth || fitWindow != true){
+				img.width = img.naturalWidth;
+				img.height = img.naturalHeight;
+			}
+			else{
+				img.width = window.innerWidth * 0.8;
+				img.height = img.naturalHeight * window.innerWidth * 0.8 / img.naturalWidth
+			}
+		}
 	}
     var _this = this;
 	this.init = function(){
-		var newNode = document.createElement("span");
+		var newNode = document.createElementNS('http://www.w3.org/1999/xhtml', 'span');
 		newNode.className = "image-viewer-fixed-element";
 		newNode.innerHTML = '<div class="btn-group">'
 		    + '<button class="btn btn-default" id="image-viewer-zoomin"><span class="glyphicon glyphicon-zoom-in"></span></button>'
 			+ '<button class="btn btn-default" id="image-viewer-zoomout"><span class="glyphicon glyphicon-zoom-out"></span></button>'
 			+ '<button class="btn btn-default" id="image-viewer-original"><span class="glyphicon glyphicon-picture"></span></button>'
 			+ '</div>';
-		document.body.insertBefore(newNode, img)
+		document.body.insertBefore(newNode, img);
 		document.getElementById("image-viewer-zoomin").onclick = this.zoomin;
 		document.getElementById("image-viewer-zoomout").onclick = this.zoomout;
 		document.getElementById("image-viewer-original").onclick = this.original;
@@ -99,13 +151,22 @@ function ImageViewer(img){
 
 (function(){
 	var defaultImg = document.getElementsByTagName('img')[0];
-  var newImg = document.createElement('img');
-  var parent = defaultImg.parentElement;
-  parent.removeChild(defaultImg);
-  parent.insertBefore(newImg, null);
-	newImg.onload = function(){
-    var imageViewer = new ImageViewer(newImg);
-	   imageViewer.init();
-  }
-  newImg.src = defaultImg.src;
+	var defaultSvg = document.getElementsByTagName('svg')[0];
+
+	if (defaultSvg) {
+		// Handle SVG element (already in proper HTML structure from loader)
+		var imageViewer = new ImageViewer(defaultSvg);
+		imageViewer.init();
+	} else if (defaultImg) {
+		// Handle IMG element
+		var newImg = document.createElement('img');
+		var parent = defaultImg.parentElement;
+		parent.removeChild(defaultImg);
+		parent.insertBefore(newImg, null);
+		newImg.onload = function(){
+			var imageViewer = new ImageViewer(newImg);
+			imageViewer.init();
+		}
+		newImg.src = defaultImg.src;
+	}
 })();
--- src/loader.js
+++ src/loader.js
@@ -1,44 +1,111 @@
-(function(){
-   function loadJs(jsPath){
-      var head= document.getElementsByTagName('head')[0];
-      var script= document.createElement('script');
-      script.type= 'text/javascript';
-      script.src= jsPath;
-      head.appendChild(script);
-   }
+(function () {
+  // Check if this is a pure SVG page and restructure if needed
+  function restructureSVGPage() {
+    if (
+      document.documentElement &&
+      document.documentElement.nodeName.toUpperCase() === "SVG"
+    ) {
+      // Save reference to the SVG root element
+      var svgRoot = document.documentElement;
 
-	function loadCss(url) {
-		var link = document.createElement("link");
-		link.type = "text/css";
-		link.rel = "stylesheet";
-		link.href = url;
-		document.getElementsByTagName("head")[0].appendChild(link);
-	}
+      // Create HTML wrapper elements
+      var html = document.createElementNS('http://www.w3.org/1999/xhtml', 'html');
+      var head = document.createElementNS('http://www.w3.org/1999/xhtml', 'head');
+      var body = document.createElementNS('http://www.w3.org/1999/xhtml', 'body');
 
-	function addLoadEvent(func) {
-		var oldonload = window.onload;
-		if (typeof window.onload != 'function') {
-			window.onload = func;
-		} else {
-			window.onload = function() {
-			if (oldonload) {
-				oldonload();
-			}
-			func();
-			}
-		}
-	}
+      // Clone the SVG to preserve it
+      var svgClone = svgRoot.cloneNode(true);
 
-	function findImage() {
-		return (document.body && document.body.childNodes.length == 1 && document.body.childNodes[0].nodeName == "IMG") ? true : false;
-	}
+      // Build structure
+      html.appendChild(head);
+      html.appendChild(body);
+      body.appendChild(svgClone);
 
-	addLoadEvent(function(){
-		if(findImage()){
-			loadCss(chrome.runtime.getURL("image-viewer/bootstrap/css/bootstrap.min.css"));
-			loadCss(chrome.runtime.getURL("image-viewer/css/image-viewer.css"));
-			loadJs(chrome.runtime.getURL("image-viewer/image-viewer.js"));
-		}
-	});
+      // Replace document root
+      var parent = svgRoot.parentNode;
+      if (parent) {
+        parent.replaceChild(html, svgRoot);
+      } else {
+        // No parent, try direct replacement
+        document.replaceChild(html, svgRoot);
+      }
 
+      return true;
+    }
+    return false;
+  }
+
+
+  function loadJs(jsPath) {
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');
+    script.type = "text/javascript";
+    script.src = jsPath;
+    head.appendChild(script);
+  }
+
+  function loadCss(url) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElementNS('http://www.w3.org/1999/xhtml', 'link');
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    head.appendChild(link);
+  }
+
+  function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != "function") {
+      window.onload = func;
+    } else {
+      window.onload = function () {
+        if (oldonload) {
+          oldonload();
+        }
+        func();
+      };
+    }
+  }
+
+  function findImage() {
+    // Check if document.documentElement is SVG (pure SVG page)
+    if (
+      document.documentElement &&
+      document.documentElement.nodeName.toUpperCase() === "SVG"
+    ) {
+      return true;
+    }
+
+    // For regular images, check if body exists
+    if (!document.body) {
+      return false;
+    }
+
+    // Filter out text nodes and only count element nodes
+    var elements = [];
+    for (var i = 0; i < document.body.childNodes.length; i++) {
+      if (document.body.childNodes[i].nodeType === 1) {
+        // ELEMENT_NODE
+        elements.push(document.body.childNodes[i]);
+      }
+    }
+
+    // Allow up to 2 elements and check if first one is IMG or SVG
+    if (elements.length >= 1 && elements.length <= 2) {
+      var nodeName = elements[0].nodeName.toUpperCase();
+      return nodeName === "IMG" || nodeName === "SVG";
+    }
+    return false;
+  }
+
+  addLoadEvent(function () {
+    if (findImage()) {
+      restructureSVGPage();
+      loadCss(
+        chrome.runtime.getURL("image-viewer/bootstrap/css/bootstrap.min.css"),
+      );
+      loadCss(chrome.runtime.getURL("image-viewer/css/image-viewer.css"));
+      loadJs(chrome.runtime.getURL("image-viewer/image-viewer.js"));
+    }
+  });
 })();
